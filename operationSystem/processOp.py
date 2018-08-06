341f1d41267d22467bbee0c13f9ef2ed633be7f1
maojianmiao <gdmaojm@163.com>
2018-08-06 13:55:40 +0800

    根据进程名关闭进程

---
+++ operationSystem/processOp.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# @Date    : 2018-08-06 11:00:12
+# @Author  : maojianmiao
+# @Link    : http://example.org
+# @Version : $Id$
+import os
+import psutil
+def killProcessByName(name):
+    for pid in psutil.pids():
+        p = psutil.Process(pid)
+        currentName = p.name()
+        if currentName == name or currentName == '{}.exe'.format(name):
+            p.terminate()
+
+
+if __name__ == '__main__':
+    killProcessByName('chromedriver')
+    killProcessByName('phantomjs')
